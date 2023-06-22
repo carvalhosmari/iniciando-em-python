8cc828fe805b41be38546babd07e2a4dd57a5d38
carvalhosmari <mscarvall@gmail.com>
2023-06-22 11:59:58 -0300

    utilizando a lib random para geração do numero aleatório que deverá ser adivinhado pelo usuário

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,42 +1,28 @@
+import random
+
 print("*************************")
 print("Jogo de adivinhação")
 print("*************************", end="\n\n")
 
-numero_secreto = 34
+numero_secreto = random.randint(1, 101)
 max_tentativas = 3
 rodada = 1
 
 #utilizando loop while
-'''
 while(rodada <= max_tentativas):
-    print("\nTentativa {} de {}:".format(rodada, max_tentativas))
+    print("Tentativa {} de {}:".format(rodada, max_tentativas))
 
     palpite = int(input("Digite um numero: "))
 
     if(numero_secreto == palpite):
-        print("Você acertou!")
+        print("Você acertou!\n")
         break
     elif(numero_secreto > palpite):
-        print("Você errou! O numero secreto é maior do que seu palpite.")
+        print("Você errou! O numero secreto é maior do que seu palpite.\n")
     else:
-        print("Você errou! O numero secreto é menor do que seu palpite.")
+        print("Você errou! O numero secreto é menor do que seu palpite.\n")
 
     rodada+= 1
 
-print("Fim de jogo!") '''
+print(f"Fim de jogo! O número que deveria ser adivinhado era {numero_secreto}")
 
-#utilizando loop for
-for tentativa in range(rodada, max_tentativas + 1, 1):
-    print("\nTentativa {} de {}:".format(tentativa, max_tentativas))
-
-    palpite = int(input("Digite um numero: "))
-
-    if (numero_secreto == palpite):
-        print("Você acertou!")
-        break
-    elif (numero_secreto > palpite):
-        print("Você errou! O numero secreto é maior do que seu palpite.")
-    else:
-        print("Você errou! O numero secreto é menor do que seu palpite.")
-
-print("Fim de jogo!")
