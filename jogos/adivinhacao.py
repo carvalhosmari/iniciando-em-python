27f6581fe449d083d9f7ae8751195d369b123076
carvalhosmari <mscarvall@gmail.com>
2023-06-22 13:40:41 -0300

    incluindo sistema de pontuacao e niveis de dificuldade

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -5,24 +5,37 @@ print("Jogo de adivinhação")
 print("*************************", end="\n\n")
 
 numero_secreto = random.randint(1, 101)
-max_tentativas = 3
+max_tentativas = 0
 rodada = 1
+pontuacao = 1000
+
+nivel = int(input("Digite o nível de dificuldade do jogo:\n(1) fácil\n(2) médio\n(3) difícil\n"))
+
+if(nivel == 1):
+    max_tentativas = 10
+elif(nivel == 2):
+    max_tentativas = 7
+elif(nivel == 3):
+    max_tentativas = 3
 
 #utilizando loop while
 while(rodada <= max_tentativas):
-    print("Tentativa {} de {}:".format(rodada, max_tentativas))
+    print("Tentativa {} de {}:".format(rodada, max_tentativas), numero_secreto)
 
     palpite = int(input("Digite um numero: "))
 
     if(numero_secreto == palpite):
-        print("Você acertou!\n")
+        print(f"Você acertou!\n")
+        pontuacao += 300
         break
     elif(numero_secreto > palpite):
         print("Você errou! O numero secreto é maior do que seu palpite.\n")
+        pontuacao -= abs(palpite - numero_secreto)
     else:
         print("Você errou! O numero secreto é menor do que seu palpite.\n")
+        pontuacao -= abs(palpite - numero_secreto)
 
     rodada+= 1
 
-print(f"Fim de jogo! O número que deveria ser adivinhado era {numero_secreto}")
+print(f"Fim de jogo! Sua pontuação atual é {pontuacao}")
 
