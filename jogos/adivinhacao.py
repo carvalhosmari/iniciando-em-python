e26f8b6dabee34c309b627d998fee954d5754738
carvalhosmari <mscarvall@gmail.com>
2023-06-26 13:24:08 -0300

    ´funcao jogar e codigo refatorado

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,41 +1,44 @@
 import random
 
-print("*************************")
-print("Jogo de adivinhação")
-print("*************************", end="\n\n")
+def jogar():
+    print("*************************")
+    print("** JOGO DE ADIVINHAÇÃO **")
+    print("*************************", end="\n\n")
 
-numero_secreto = random.randint(1, 101)
-max_tentativas = 0
-rodada = 1
-pontuacao = 1000
+    numero_secreto = random.randint(1, 101)
+    max_tentativas = 0
+    rodada = 1
+    pontuacao = 1000
 
-nivel = int(input("Digite o nível de dificuldade do jogo:\n(1) fácil\n(2) médio\n(3) difícil\n"))
+    nivel = int(input("Digite o nível de dificuldade do jogo:\n(1) fácil (2) médio (3) difícil\n"))
 
-if(nivel == 1):
-    max_tentativas = 10
-elif(nivel == 2):
-    max_tentativas = 7
-elif(nivel == 3):
-    max_tentativas = 3
+    if(nivel == 1):
+        max_tentativas = 10
+    elif(nivel == 2):
+        max_tentativas = 7
+    elif(nivel == 3):
+        max_tentativas = 3
 
-#utilizando loop while
-while(rodada <= max_tentativas):
-    print("Tentativa {} de {}:".format(rodada, max_tentativas), numero_secreto)
+    #utilizando loop while
+    while(rodada <= max_tentativas):
+        print("Tentativa {} de {}:".format(rodada, max_tentativas))
 
-    palpite = int(input("Digite um numero: "))
+        palpite = int(input("Digite um numero: "))
 
-    if(numero_secreto == palpite):
-        print(f"Você acertou!\n")
-        pontuacao += 300
-        break
-    elif(numero_secreto > palpite):
-        print("Você errou! O numero secreto é maior do que seu palpite.\n")
-        pontuacao -= abs(palpite - numero_secreto)
-    else:
-        print("Você errou! O numero secreto é menor do que seu palpite.\n")
-        pontuacao -= abs(palpite - numero_secreto)
+        if(numero_secreto == palpite):
+            print(f"Você acertou!\n")
+            pontuacao += 300
+            break
+        elif(numero_secreto > palpite):
+            print("Você errou! O numero secreto é maior do que seu palpite.\n")
+            pontuacao -= abs(palpite - numero_secreto)
+        else:
+            print("Você errou! O numero secreto é menor do que seu palpite.\n")
+            pontuacao -= abs(palpite - numero_secreto)
 
-    rodada+= 1
+        rodada+= 1
 
-print(f"Fim de jogo! Sua pontuação atual é {pontuacao}")
+    print(f"Fim de jogo! O numero secreto é {numero_secreto}. Sua pontuação final é {pontuacao}")
 
+if(__name__ == "__main__"):
+    jogar()
