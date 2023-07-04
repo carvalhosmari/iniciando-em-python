746b31e87c7b1f841a0c0bff054ae95926f4c582
carvalhosmari <mscarvall@gmail.com>
2023-07-04 12:44:09 -0300

    finalizacao da logica do jogo e implementacao dos requisitos para vitoria ou derrota

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,21 +5,21 @@ def jogar():
 
     acertos = 0
     max_tentativas = 6
-    palavra = "exemplo"
+    palavra = "paralelepipedo"
     forca = []
     acertou = False
     excedeu = False
 
-    for letra in palavra:
+    for caracter in range(0, len(palavra)):
         forca.append("_ ")
 
     while(not acertou and not excedeu):
         for elemento in forca:
-            print(elemento, end="")
+            print(elemento.upper(), end="")
 
-        print(f"\nVocê possui {max_tentativas} tentativas. {acertos}\n")
+        print(f"\n\nVocê possui {max_tentativas} tentativas.")
 
-        tentativa = input("\nDigite uma letra: ").lower()
+        tentativa = input("\nDigite uma letra: ").lower().strip()
 
         if(palavra.count(tentativa) != 0):
             for index, elem in enumerate(palavra):
@@ -28,13 +28,13 @@ def jogar():
                     acertos += 1
             if(acertos >= len(palavra)):
                 acertou = True
-                print("Parabens! Você ganhou!!!")
+                print("\nParabens! Você ganhou!!!")
         else:
             max_tentativas -= 1
 
             if(max_tentativas == 0):
                 excedeu = True
-                print("Perdeu otarioooooo!")
+                print(f"\nPerdeu otarioooooo! A palavra era: {palavra}")
 
 if(__name__ == "__main__"):
     jogar()
